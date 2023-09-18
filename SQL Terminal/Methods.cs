ac4eecd90747c671284b80d25648ff7c3a3bc121
wesgl256 <wesgl@WORKSTATION-256>
2023-09-18 00:55:21 -0500

    added the SQL Terminal program, this is a c# program that can interact with the database with a CLI

---
+++ SQL Terminal/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SQL_Terminal {
+    public class Methods {
+
+        
+    }
+    public class SQL {
+        public MySqlConnection Connection;
+        public string IP_Address;
+        public int Port;
+        public string Database;
+        public string Username;
+        public string Password;
+
+        public SQL(string ip_address, int port, string database, string username, string password) {
+            this.IP_Address = ip_address;
+            this.Port = port;
+            this.Database = database;
+            this.Username = username;
+            this.Password = password;   
+        }
+        public void Connect() {
+            string connectionString = $"Server={this.IP_Address};Database={this.Database};Uid={this.Username};Pwd={this.Password}";
+            this.Connection = new MySqlConnection(connectionString);
+            this.Connection.Open();
+
+        }
+    }
+}
---
+++ SQL Terminal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace SQL_Terminal {
+    public class Program {
+        static void Main(string[] args) {
+            SQL sql = new SQL("10.0.0.139", 3306, "thefacebook", "terminal", "");
+            sql.Connect();
+
+            Console.WriteLine("You have successfully connected");
+
+        }
+    }
+}

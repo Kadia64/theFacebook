7a32451d1652f4745aa05c401d976cb31adeaf29
kadia-server139 <wes.glen.66@icloud.com>
2023-09-19 01:47:21 -0500

    defined some PHP functions for some styles and content of the page

--- Pages/Logged Out Pages/Welcome.php
+++ Pages/Logged Out Pages/Welcome.php
@@ -2,14 +2,21 @@
     $path = '/Projects/TheFacebook/Git/thefacebook/Server Functions/';
     require_once $_SERVER['DOCUMENT_ROOT'] . $path . 'Scripts/content.php';
     $content = new Content();
+    $styles = new Styles();
     session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>    
     <?php $content->Startup('Welcome'); ?>
+    <?php $styles->WelcomeStyle(); ?>    
+    <style>
+
+    </style>
 </head>
 <body>
-    
+    <div class="main-pagebox">
+        
+    </div>
 </body>
 </html>
--- Server Functions/Scripts/content.php
+++ Server Functions/Scripts/content.php
@@ -5,9 +5,22 @@ class Content {
         echo '
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>[ theFacebook ] ' . $title . '</title>
             <link rel="icon" type="image/png" href="/Projects/TheFacebook/Git/theFacebook/Images/facebook-icon.ico">
+            <link rel="stylesheet" href="/Projects/TheFacebook/Git/theFacebook/main-style.css">
+            <title>[ theFacebook ] ' . $title . '</title>
         ';
     }
+    public function TopContent() { }
+    public function BottomContent() { }
+    public function LeftLoginForm() { }
+    public function LeftLoginLinks() { }
+}
+class Styles {
+    public function LoginStyle() { }
+    public function RegisterAboutUserStyle() { }
+    public function RegisterUserStyle() { }
+    public function WelcomeStyle() { }
+    public function EnableCookiesStyle() { }
+    public function MainProfilePageStyle() { }    
 }
 ?>

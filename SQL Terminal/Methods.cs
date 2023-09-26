86b6f1389cd1a78b1506e4feefd7ef93f4806abb
kadia-server139 <wes.glen.66@icloud.com>
2023-09-26 03:45:22 -0500

    created some more commands and you can clear and rebuild the structure of the database

--- SQL Terminal/Methods.cs
+++ SQL Terminal/Methods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,6 +71,12 @@ namespace SQL_Terminal {
 
             Console.WriteLine();
         }
+        public void SuccessOutput(string input, bool print = false) {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine();
+        }
         public void ErrorOutput(string input, bool print = false) {
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Red;
@@ -80,6 +87,26 @@ namespace SQL_Terminal {
             }
             Console.ResetColor();
             Console.WriteLine("\n");
+        }
+        public bool Hault(string text, ConsoleColor color = ConsoleColor.White) {
+            bool read = true;
+            while (true) {
+                Console.ForegroundColor = color;                
+                if (read) this.Print("\n" + text + "\n");
+                read = false;
+                Console.Write("   [y/n]\n\n");
+                Console.ResetColor();
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                
+                if (input == "y" || input == "Y") {
+                    return true;
+                } else if (input == "n" || input == "N") {
+                    return false;
+                } else {
+                    Console.WriteLine("\nTry Again\n");
+                }                
+            }
         }
         public void Print(string text, int speed = 10) {
             foreach (char c in text) {
@@ -115,6 +142,112 @@ namespace SQL_Terminal {
             if (this.Connection.State == System.Data.ConnectionState.Open) {
                 return true;
             } else return false;                
+        }
+        public void Query(string query_string) {
+            using (MySqlCommand command = new MySqlCommand(query_string, this.Connection)) {
+                command.ExecuteNonQuery();
+            }
+        }
+        public void ClearDatabase() {
+            List<string> tableNames = new List<string>();
+            using (DataTable dt = this.Connection.GetSchema("Tables")) {
+                foreach (DataRow row in dt.Rows) {
+                    string tableName = row["TABLE_NAME"].ToString();
+                    tableNames.Add(tableName);
+                }
+            }
+            foreach (string tableName in tableNames) {
+                string dropTable = $"DROP TABLE {tableName}";
+                this.Query(dropTable);
+            }
+        }
+
+        public void CreateDefaultStructure() {
+            this.Query(@"
+                CREATE TABLE account_settings (
+  	                settings_id INT PRIMARY KEY AUTO_INCREMENT,
+  	                allow_mentions VARCHAR(255),
+  	                activity_status BOOLEAN,
+  	                suggest_account BOOLEAN
+                );
+                CREATE TABLE account_stats (
+                    stats_id INT PRIMARY KEY AUTO_INCREMENT,
+                    login_count INT UNSIGNED,
+                    logout_count INT UNSIGNED,
+                    last_login_timestamp VARCHAR(32),
+                    last_logout_timestamp VARCHAR(32),
+                    password_attempts INT UNSIGNED,
+                    last_password_attempt_timestamp VARCHAR(32),
+                    password_change_count INT UNSIGNED,
+                    last_password_changed_timestamp VARCHAR(32),
+                    member_since VARCHAR(32),
+                    member_since_time VARCHAR(32),
+                    last_update VARCHAR(32),
+                    last_update_time VARCHAR(32),
+                    friend_count INT UNSIGNED,
+                    friend_email_list VARCHAR(512),
+                    blocked_count INT UNSIGNED,
+                    blocked_username_list VARCHAR(512),
+                    reported_count INT UNSIGNED,
+                    message_all_count INT UNSIGNED,
+                    unread_message_count INT UNSIGNED, 
+                    message_sent_count INT UNSIGNED, 
+                    message_received_count INT UNSIGNED, 
+                    verification_request_count INT UNSIGNED, 
+                    verification_request_last_timestamp VARCHAR(32)
+                );
+                CREATE TABLE personal_info (
+                    personal_info_id INT PRIMARY KEY AUTO_INCREMENT,
+                    first_name VARCHAR(128),
+                    last_name VARCHAR(128),
+                    birthday DATE,
+                    sex VARCHAR(32),
+                    address VARCHAR(128),
+                    home_town VARCHAR(128),
+                    highschool VARCHAR(128),
+                    education_status VARCHAR(32),
+                    website VARCHAR(128),
+                    looking_for VARCHAR(32),
+                    interested_in VARCHAR(32),
+                    relationship_status VARCHAR(32),
+                    political_views VARCHAR(128),
+                    interests VARCHAR(128),
+                    favorite_music TEXT,
+                    favorite_movies TEXT,
+                    about_me TEXT
+                );
+                CREATE TABLE account_info (
+                    account_id INT PRIMARY KEY AUTO_INCREMENT,
+                    settings_id INT, 
+                    stats_id INT, 
+                    personal_info_id INT,
+                    username VARCHAR(128),
+                    email VARCHAR(128),
+                    password VARCHAR(128),    
+                    mobile VARCHAR(128),
+                    full_name VARCHAR(128),    
+                    CONSTRAINT account_settings_fk FOREIGN KEY (settings_id) 
+                   		REFERENCES account_settings (settings_id),
+                    CONSTRAINT account_stats_fk FOREIGN KEY (stats_id)
+                    	REFERENCES account_stats (stats_id),
+                    CONSTRAINT personal_info_fk FOREIGN KEY (personal_info_id)
+                    	REFERENCES personal_info (personal_info_id)
+                );
+            ");
+        }
+        public void TruncateAllRelationalTables() {
+            this.Query(@"
+                ALTER TABLE account_info DROP FOREIGN KEY account_settings_fk;
+                ALTER TABLE account_info DROP FOREIGN KEY account_stats_fk;
+                ALTER TABLE account_info DROP FOREIGN KEY personal_info_fk;
+                TRUNCATE TABLE account_settings;
+                TRUNCATE TABLE account_stats;
+                TRUNCATE TABLE personal_info;
+                TRUNCATE TABLE account_info;
+                ALTER TABLE account_info ADD CONSTRAINT account_settings_fk FOREIGN KEY (settings_id) REFERENCES account_settings(settings_id);
+                ALTER TABLE account_info ADD CONSTRAINT account_stats_fk FOREIGN KEY (stats_id) REFERENCES account_stats(stats_id);
+                ALTER TABLE account_info ADD CONSTRAINT personal_info_fk FOREIGN KEY (personal_info_id) REFERENCES personal_info(personal_info_id);
+            ");
         }
     }
 }
--- SQL Terminal/Run.cs
+++ SQL Terminal/Run.cs
@@ -9,13 +9,16 @@ namespace SQL_Terminal {
     public class Run {
 
         private string[] CommandList = {
-            "commands", "list commands", "cls",
-            "connect", "con", "disconnect"
+            "commands", "list commands", "cmds", "clear", "cls", "exit",
+            "connect", "con", "disconnect",
+            "cleardb", "clear databases", "remove tables", "delete tables",
+            "create default",
         };
         private bool Connected = false;
         private string? CurrentCommand = null;
         private string? CommandInput = null;
         private bool Help = false;
+        private bool Skip = false;
         private const string HELP_INFO = "-h ~ help mode, will give a description of the command.";
         private const string SKIP_INFO = "-f ~ will execute the command without a prompt.";
 
@@ -23,33 +26,65 @@ namespace SQL_Terminal {
             Methods methods = new Methods();
             SQL sql = new SQL("10.0.0.139", 3306, "thefacebook", "terminal", "");
             bool running = true;
-            bool skip = false;
             string[] inputTokens = new string[] { };
             ConsoleColor color = ConsoleColor.White;
 
             while (running) {
-                methods.PrintCursor();
+                if (!Connected) {
+                    methods.PrintCursor(ConsoleColor.Red);
+                } else {
+                    methods.PrintCursor(ConsoleColor.Blue);
+                }
                 string cmd = Console.ReadLine();
                 bool pass = false;                
                 if (this.CheckCommand(cmd)) pass = true;
                 
                 try {
                     inputTokens = this.CommandInput.Split(' ');
-                    if (inputTokens.Contains("-f")) skip = true;
+                    if (inputTokens.Contains("-f")) this.Skip = true;
                     if (inputTokens.Contains("-h")) this.Help = true;
                 } catch { pass = false; }
                 if (!pass) methods.ErrorOutput("Unexpected Error", true);
-                
+
+                bool execute = false;
                 switch (this.CurrentCommand) {
-                    case "connect":                        
-                    case "con":
+                    case "clear":
+                    case "cls":
+                        if (this.Help) {
+                            methods.HelpOutput("Clears the screen of the console.", new string[] { HELP_INFO }, new string[] { "clear", "cls" });
+                            break;
+                        }
+                        Console.Clear();
+                        break;
+                    case "exit":
+                        if (this.Help) {
+                            methods.HelpOutput("Exists the console program.", new string[] { HELP_INFO }, new string[] { "exit" });
+                            break;
+                        }
+                        Environment.Exit(0);
+                        break;
+                    case "list commands":
+                    case "cmds":
+                        if (this.Help) {
+                            methods.HelpOutput("Will give a list of each command displayed to the console", new string[] { HELP_INFO }, new string[] { "list commands", "cmds" });
+                            break;
+                        }
+                        Console.WriteLine();
+                        for (int i = 0; i < this.CommandList.Length; ++i) {
+                            Console.WriteLine(CommandList[i]);
+                            Thread.Sleep(10);
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "connect":
+                    case "con":                        
                         if (this.Help) {
                             methods.HelpOutput("Connects the user to the database, the configuration will be in the config.json file.\nYou must restart the terminal for changes to apply.", new string[] { HELP_INFO, SKIP_INFO }, new string[] { "connect", "con" });
                             break;
                         }
                         try {
-                            sql.Connect();
-                            methods.CommandOutput($"Connected to the SQL database '{sql.Database}' on {sql.IP_Address}:{sql.Port} - {DateTime.Now.ToString("M/d/yy h:mm tt")}", true, ConsoleColor.Cyan);
+                            sql.Connect();                            
+                            if (!this.Skip) methods.CommandOutput($"Connected to the SQL database '{sql.Database}' on {sql.IP_Address}:{sql.Port} - {DateTime.Now.ToString("M/d/yy h:mm tt")}", true, ConsoleColor.Cyan);
                             this.Connected = true;
                         } catch (Exception e) {
                             methods.CommandOutput($"Connection Failed: {e.Message}");
@@ -63,15 +98,41 @@ namespace SQL_Terminal {
                         if (this.Connected) {
                             sql.CloseConnection();
                             if (!sql.CheckConnection()) {
-                                methods.CommandOutput($"Disconnected from the SQL database - {sql.IP_Address}:{sql.Port}");
+                                if (!this.Skip) methods.CommandOutput($"Disconnected from the SQL database - {sql.IP_Address}:{sql.Port}");
                                 this.Connected = false;
                             }
                         } else {
                             methods.ErrorOutput("You are currently not connected to a SQL database!", true);
                         }
                         break;
+                    case "cleardb":
+                    case "clear databases":
+                    case "remove tables":
+                    case "delete tables":
+                        if (this.Help) {
+                            methods.HelpOutput("This will delete all of your tables from your database, and all of their data.", new string[] { HELP_INFO, SKIP_INFO }, new string[] { "cleardb", "clear databases", "remove tables", "delete tables" });
+                            break;
+                        }                        
+                        if (!this.Skip) {
+                            if (this.Connected) {
+                                if (methods.Hault("Are you sure your would like to delete all tables within your database?", ConsoleColor.Red)) execute = true;
+                            } else methods.ErrorOutput("You are not connected to the database!");
+                        }
+                        if (execute || this.Skip) sql.ClearDatabase();
+                        break;
+                    case "create default":
+                        if (this.Help) {
+                            methods.HelpOutput("Will create a default structure for your database, and it will include the related tables.\nThis will delete all existing data fields from your database?", new string[] { HELP_INFO, SKIP_INFO }, new string[] { "create default" });
+                            break;
+                        }
+                        if (!this.Skip) {
+                            if (this.Connected) {
+                                if (methods.Hault("Are you sure you want to create a new database structure? It will delete all existing data in the database.", ConsoleColor.Red)) execute = true;                                
+                            } else methods.ErrorOutput("You are not connected to the database!");
+                        }
+                        if (execute || this.Skip) sql.CreateDefaultStructure();
+                        break;
                 }
-
                 this.Reset();
             }
         }
@@ -93,6 +154,7 @@ namespace SQL_Terminal {
             this.CommandInput = null;
             this.CurrentCommand = null;
             this.Help = false;
+            this.Skip = false;
         }
         
     }
